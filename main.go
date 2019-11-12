ebb1449d5613d4c1e393cfc14e0eba280dfc9667
svwielga4 <svwielga4@gmail.com>
2019-11-12 14:55:00 -0800

    adds html site grabbing

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 func main() {
 	urlFlag := flag.String("url", "https://stanwielga.com", "the url you want to build a sitemap for")
 	flag.Parse()
-	fmt.Println(*urlFlag)
 
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
 	/*
 		get the webpage
 		parse all the links on the page
